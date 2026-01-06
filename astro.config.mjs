fe1aca8caae12ec06e837714f960fd3bf93b87f1
EtoDemerzel0427 <huangweiran1998@gmail.com>
2026-01-06 01:00:05 -0600

    fix site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,17 @@ import tailwind from '@astrojs/tailwind';
 
 import partytown from '@astrojs/partytown';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
     // deployed to https://<username>.github.io/
-    site: 'https://etodemerzel0427.github.io',
+    site: 'https://huangweiran.club',
     base: '/',
     integrations: [react({
         include: ['**/react/*'], // Optional: explicit inclusion if needed, usually auto-detected
     }), tailwind({
         // Example: Disable default base styles if needed, but we likely want them
         applyBaseStyles: false,
-    }), partytown()],
+    }), partytown(), sitemap()],
 });
